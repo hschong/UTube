888dbbf45713b09da2f7a9d84a44e19d10f97531
Heeseok Chong <heeseok.chong@gmail.com>
2019-02-11 16:12:13 +0700

    added the differences of export between default and variable.

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -4,5 +4,22 @@ const globalRouter = express.Router();
 
 export default globalRouter
 
+/*
+// In source.js
+const source  = () => console.log("hi");
+export default source;
+
+// In target.js
+import source from "./source.js";
+
+// Whatever else is inside source.js is not gonna be exported by default.
 
 
+// If you don't wanna export one thing as a default and you want to export many things.
+export const a = () => console.log("hi a");
+export const b = () => console.log("hi b");
+export const c = () => console.log("hi c");
+
+// In target.js
+import { a, b, c} from "./source";
+*/
