6bb6d12f13bb6d81cccb3260ff3d78b4827750da
Heeseok Chong <heeseok.chong@gmail.com>
2019-02-19 18:03:01 +0700

    using dotenv to hide mongo DB address.

--- db.js
+++ db.js
@@ -51,9 +51,12 @@
 
 
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 mongoose.connect(
-    "mongodb://localhost:27017/UTube",
+    // "mongodb://localhost:27017/UTube",
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true,
         useFindAndModify: false
