9a069a6a580a35a9de8855a9ba3f1ca72b7fcba9
Heeseok Chong <heeseok.chong@gmail.com>
2019-02-19 19:01:11 +0700

    added video scheme.

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 import "./db"
 import app from "./app";
 import dotenv from "dotenv";
+import "./models/video";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
---
+++ models/video.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+
+const VideoScheme = new mongoose.Schema({
+    fileURL: {
+        type: String,
+        required: "File URL is required"
+    },
+    title: {
+        type: String,
+        required: "Title is required"
+    },
+    description: String,
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const model = mongoose.model("Video", VideoScheme);
+export default model;
